3ba4f69899c184b04a2740ed5085063f1e944ef4
James Serra <Jamesserra4@gmail.com>
2022-08-20 12:37:15 -0400

    search filter

--- src/componets/AddExpenseForm.js
+++ src/componets/AddExpenseForm.js
@@ -20,6 +20,8 @@ const AddExpenseForm = () => {
       type: "ADD_EXPENSE",
       payload: expense,
     });
+    setName("");
+    setCost("");
   };
 
   return (
--- src/componets/ExpenseList.js
+++ src/componets/ExpenseList.js
@@ -1,21 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import { AppContext } from "../context/AppContext";
 
 const ExpenseList = () => {
   const { expenses } = useContext(AppContext);
+  const [input, setInput] = useState("");
+  const searchFilter = expenses.filter((expense) =>
+    expense.name.toLowerCase().includes(input)
+  );
 
   return (
-    <ul className="list-group">
-      {expenses.map((expense) => (
-        <ExpenseItem
-          id={expense.id}
-          name={expense.name}
-          cost={expense.cost}
-          key={expense.id}
-        />
-      ))}
-    </ul>
+    <>
+      <input
+        type="text"
+        class="form-control mb-2 mr-sm-2"
+        placeholder="Type to search..."
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <ul className="list-group">
+        {searchFilter.length > 0
+          ? searchFilter.map((expense) => (
+              <ExpenseItem
+                id={expense.id}
+                name={expense.name}
+                cost={expense.cost}
+                key={expense.id}
+              />
+            ))
+          : expenses.map((expense) => (
+              <ExpenseItem
+                id={expense.id}
+                name={expense.name}
+                cost={expense.cost}
+                key={expense.id}
+              />
+            ))}
+      </ul>
+    </>
   );
 };
 
